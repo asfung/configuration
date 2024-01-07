0d6cf27ff3cf124027e0b346588045ab241642e5
Paung <paungcuy15@gmail.com>
2024-01-07 20:50:03 +0700

    removing comment

--- tmuxbash.sh
+++ tmuxbash.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-#if ! command -v tmux &> /dev/null; then
-#    echo "tmux is not installed. Please install tmux before using this script."
-#    exit 1
-#fi
 
 read -p "session name: " session_name
-
 tmux new -s "$session_name" -c /bin/bash bash
-
 tmux attach -t "$session_name"
 
