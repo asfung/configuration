368b3a4424d6f1e62139ae431e32f1b6ff506d17
Paung <129583516+asfung@users.noreply.github.com>
2024-03-22 18:39:13 +0700

    Lsp default lsp issue brooopp

--- nvim/lua/paung/plugin/lsp.lua
+++ nvim/lua/paung/plugin/lsp.lua
@@ -19,7 +19,7 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {'tsserver', 'rust_analyzer', 'lua-language-server', 'clangd'},
+    ensure_installed = {'tsserver', 'rust_analyzer'},
     handlers = {
         lsp_zero.default_setup,
         lua_ls = function()
