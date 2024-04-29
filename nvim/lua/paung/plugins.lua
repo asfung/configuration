72b63ab2198cabdb0852861cfb26f403cd374a5d
Paung <paungcuy15@gmail.com>
2024-04-29 16:47:50 +0700

    remove inline conf

--- nvim/lua/paung/plugins.lua
+++ nvim/lua/paung/plugins.lua
@@ -100,36 +100,6 @@ return require('packer').startup(function(use)
         dependencies = {
             "nvim-tree/nvim-web-devicons",
         },
-
-        -- config = function()
-        --     local alpha = require("alpha")
-        --     local dashboard = require("alpha.themes.startify")
-        --         --vim.cmd("hi dashboardheader guifg=#4caf50") -- bad habit cause forcing the general startup, i think so
-        --       dashboard.section.header.val = {
-        --           '                     .:::!~!!!!!:.',
-        --           '                  .xuhwh!! !!?m88whx:.         _____      __    __    _____    _____       ________     ____     __    __     ____    ',
-        --           '                .x*#m@$!!  !x!m$$$$$$wwx:.    (_   _)     ) )  ( (   / ____\\  / ___/      (___  ___)   (    )    ) )  ( (    (    )   ',
-        --           '               :!!!!!!?h! :!$!$$$$$$$$$$8x:     | |      ( (    ) ) ( (___   ( (__            ) )      / /\\ \\   ( (    ) )   / /\\ \\   ',
-        --           '              !!~  ~:~!! :~!$!#$$$$$$$$$$8x:    | |       ) )  ( (   \\___ \\   ) __)          ( (      ( (__) )   \\ \\  / /   ( (__) )  ',
-        --           '             :!~::!h!<   ~.u$x!?r$$$$$$$$mm!    | |      ( (    ) )      ) ) ( (          __  ) )      )    (     \\ \\/ /     )    (   ',
-        --           '             ~!~!!!!~~ .:xw$$$u!!?$$$$$$rmm!   _| |__     ) \\__/ (   ___/ /   \\ \\___     ( (_/ /      /  /\\  \\     \\  /     /  /\\  \\  ',
-        --           '               !:~~~ .:!m"t#$$$$wx??#mrrmmm!  /_____(     \\______/  /____/     \\____\\     \\___/      /__(  )__\\     \\/     /__(  )__\\ ',
-        --           '               ~?wuxiw*`   `"#$$$$8!!!!??!!!',
-        --           '             :x- m$$$$       `"t#$t~!8$wuxu~',
-        --           '            :%`  ~#$$$m:        ~!~ ?$$$$$$',
-        --           '          :!`.-   ~t$$$$8xx.  .xww- ~""##*"',
-        --           '.....   -~~:<` !    ~?t#$$@@w@*?$$      /`',
-        --           'w$@@m!!! .!~~ !!     .:xuw$w!~ `"~:    :',
-        --           '#"~~`.:x%`!!  !h:   !wm$$$$ti.: .!wun+!`',
-        --           ':::~:!!`:x~ .: ?h.!u "$$$b$$$!w:u!t$$m~',
-        --           '.~~   :x@!.-~   ?@wtwo("*$$$w$th$! `',
-        --           'wi.~!x$?!-~   :: ?$$$b$wu("**$rm!',
-        --           '$r@i.~~ !    ::   ~$$$$$b$$en:``',
-        --           '?mxt@wx.~   ::     ~"##*$$$$m'
-        --       }
-        --
-        --     alpha.setup(dashboard.opts)
-        -- end,
     }
 
     use "windwp/nvim-autopairs"
