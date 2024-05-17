1b67a193723dcc6c934d3f67ffaef3c67aee046f
Paung <paungcuy15@gmail.com>
2024-05-17 17:53:08 +0700

    setup nvim for linux/unix only

---
+++ setup_nvim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone --filte=blob:none --no-checkout git@github.com:asfung/configuration.git ~/configuration && cd ~/configuration
+git sparse-checkout set --cone
+git checkout main
+git sparse-checkout set nvim
+
+cp -r ~/configuration/nvim ~/.config/
+# cp -r ~/configuration/nvim ~/sini # testing
+
+
